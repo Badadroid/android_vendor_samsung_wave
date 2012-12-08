f71f696b33f5029410eb5666ebef61219b6c99ec
Dominik Marszk <dmarszk@gmail.com>
2012-12-08 12:57:36 +0000

    Renamed repo from wave-common to wave.

--- wave-vendor-blobs.mk
+++ wave-vendor-blobs.mk
@@ -14,29 +14,29 @@
 
 # All the blobs necessary for galaxys devices
 PRODUCT_COPY_FILES += \
-    vendor/samsung/wave-common/proprietary/pvrsrvinit:system/vendor/bin/pvrsrvinit \
-    vendor/samsung/wave-common/proprietary/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
-    vendor/samsung/wave-common/proprietary/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
-    vendor/samsung/wave-common/proprietary/samsung_mfc_fw.bin:system/vendor/firmware/samsung_mfc_fw.bin \
-    vendor/samsung/wave-common/proprietary/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/samsung/wave-common/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
-    vendor/samsung/wave-common/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
-    vendor/samsung/wave-common/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
-    vendor/samsung/wave-common/proprietary/gralloc.wave.so:system/vendor/lib/hw/gralloc.wave.so \
-    vendor/samsung/wave-common/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
-    vendor/samsung/wave-common/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
-    vendor/samsung/wave-common/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
-    vendor/samsung/wave-common/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
-    vendor/samsung/wave-common/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
-    vendor/samsung/wave-common/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
-    vendor/samsung/wave-common/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
-    vendor/samsung/wave-common/proprietary/libusc.so:system/vendor/lib/libusc.so \
-    vendor/samsung/wave-common/proprietary/lib_tvoutengine.so:system/lib/lib_tvoutengine.so \
-    vendor/samsung/wave-common/proprietary/libtvout.so:system/lib/libtvout.so \
-    vendor/samsung/wave-common/proprietary/libtvout_jni.so:system/lib/libtvout_jni.so \
-    vendor/samsung/wave-common/proprietary/libtvoutfimc.so:system/lib/libtvoutfimc.so \
-    vendor/samsung/wave-common/proprietary/libtvouthdmi.so:system/lib/libtvouthdmi.so \
-    vendor/samsung/wave-common/proprietary/libtvoutservice.so:system/lib/libtvoutservice.so \
-    vendor/samsung/wave-common/proprietary/libakm.so:system/vendor/lib/libakm.so \
-    vendor/samsung/wave-common/proprietary/tvoutserver:system/bin/tvoutserver \
-    vendor/samsung/wave-common/proprietary/orientationd:system/vendor/bin/orientationd
+    vendor/samsung/wave/proprietary/pvrsrvinit:system/vendor/bin/pvrsrvinit \
+    vendor/samsung/wave/proprietary/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
+    vendor/samsung/wave/proprietary/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
+    vendor/samsung/wave/proprietary/samsung_mfc_fw.bin:system/vendor/firmware/samsung_mfc_fw.bin \
+    vendor/samsung/wave/proprietary/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    vendor/samsung/wave/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/samsung/wave/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/samsung/wave/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/samsung/wave/proprietary/gralloc.wave.so:system/vendor/lib/hw/gralloc.wave.so \
+    vendor/samsung/wave/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+    vendor/samsung/wave/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+    vendor/samsung/wave/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+    vendor/samsung/wave/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+    vendor/samsung/wave/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+    vendor/samsung/wave/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+    vendor/samsung/wave/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+    vendor/samsung/wave/proprietary/libusc.so:system/vendor/lib/libusc.so \
+    vendor/samsung/wave/proprietary/lib_tvoutengine.so:system/lib/lib_tvoutengine.so \
+    vendor/samsung/wave/proprietary/libtvout.so:system/lib/libtvout.so \
+    vendor/samsung/wave/proprietary/libtvout_jni.so:system/lib/libtvout_jni.so \
+    vendor/samsung/wave/proprietary/libtvoutfimc.so:system/lib/libtvoutfimc.so \
+    vendor/samsung/wave/proprietary/libtvouthdmi.so:system/lib/libtvouthdmi.so \
+    vendor/samsung/wave/proprietary/libtvoutservice.so:system/lib/libtvoutservice.so \
+    vendor/samsung/wave/proprietary/libakm.so:system/vendor/lib/libakm.so \
+    vendor/samsung/wave/proprietary/tvoutserver:system/bin/tvoutserver \
+    vendor/samsung/wave/proprietary/orientationd:system/vendor/bin/orientationd
